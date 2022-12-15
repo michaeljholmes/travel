9a3298ae65177e10b09e86630bbbbd545820346c
mikeholmes767 <mikeholmes767@penguin>
2022-12-15 17:31:42 +0000

    update to git ignore

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">Travel Blog Here we come!</div>;
+  return <div className="App">Travel Blog Here we come! On Chrome book!</div>;
 }
 
 export default App;
