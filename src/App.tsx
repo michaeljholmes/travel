9325d27df08587bd5cece379ce54e4bf4198e63d
michaeljholmes <mikeh80@hotmail.co.uk>
2022-12-30 08:51:24 -0600

    Adding packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+// @ts-ignore
+import { ReactLeafletWaypoints } from 'react-leaflet-waypoints';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">Travel Blog Here we come! On Chrome book! Pushed</div>
+    <div className="App">
+      <ReactLeafletWaypoints
+        layerUrl="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        waypoints={[
+          { lat: '40.4381311', lng: '-3.8196196' },
+          { lat: '42.7576862', lng: '1.5082874' },
+        ]}
+      />
+    </div>
   );
 }
 
