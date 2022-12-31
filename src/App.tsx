6ca22a154ec7e1ff2a55ff94ce72407493e0b4c8
michaeljholmes <mikeh80@hotmail.co.uk>
2022-12-31 13:42:25 -0600

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,20 @@ import { ReactLeafletWaypoints } from 'react-leaflet-waypoints';
 import { styled } from '@mui/material';
 import { Map } from './components/Map';
 import { CustomAppBar } from './components/AppBar';
+import { ItineraryAccordion } from './components/ItineraryAccordion';
 
 export const App = () => {
   return (
     <div className="App">
       <CustomAppBar />
+      <ItineraryAccordion
+        itinerary={[
+          {
+            title: 'place 1',
+            description: 'Some desc',
+          },
+        ]}
+      />
       <Map
         waypoints={[
           { lat: '21.1619', lng: '-86.8515' },
--- src/components/ItineraryAccordian.tsx
+++ src/components/ItineraryAccordian.tsx
@@ -20,45 +20,20 @@ interface ItineraryAccordianProps {
 export const ItineraryAccordion = ({ itinerary }: ItineraryAccordianProps) => {
   return (
     <div>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Accordion 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion disabled>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography>Disabled Accordion</Typography>
-        </AccordionSummary>
-      </Accordion>
+      {itinerary.map(({ title, description }) => (
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>{title}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{description}</Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </div>
   );
 };
