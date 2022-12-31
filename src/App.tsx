d5c5b73b8d6c32764d5d7311815c9176e1d46633
michaeljholmes <mikeh80@hotmail.co.uk>
2022-12-31 13:01:32 -0600

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 import { ReactLeafletWaypoints } from 'react-leaflet-waypoints';
 import { styled } from '@mui/material';
