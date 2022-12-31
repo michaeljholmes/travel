e8a170397c4b506b48c0b4ab51a7760d55b5ade6
michaeljholmes <mikeh80@hotmail.co.uk>
2022-12-30 21:11:17 -0600

    3 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 // @ts-ignore
 import { ReactLeafletWaypoints } from 'react-leaflet-waypoints';
+import { styled } from '@mui/material';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <ReactLeafletWaypoints
+      <Map
         layerUrl="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         waypoints={[
           { lat: '40.4381311', lng: '-3.8196196' },
@@ -21,3 +22,8 @@ function App() {
 }
 
 export default App;
+
+const Map = styled(ReactLeafletWaypoints)`
+  width: 500px;
+  height: 500px;
+`;
